89d65abd0d8160ff2753a474478a1d4596356a1e
blcoussama <belcadioussama.eng@gmail.com>
2025-02-28 10:35:22 +0000

    Adding the the api posts route.ts FILE

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,22 @@
+import { prisma } from "@/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+
+    const searchParams = request.nextUrl.searchParams;
+
+    const userProfileId = searchParams.get("userProfileId");
+
+    const { userId } = await auth(); 
+    if(!userId) return;
+
+    const whereCondition = userProfileId ? { parentPostId: null, userId: userProfileId } : {
+    parentPostId: null,
+    userId: {
+    in:[userId,...(await prisma.follow.findMany({ where: {followerId: userId}, select: {followingId: true} })).map(follow => follow.followingId)]
+    }}
+    const posts = await prisma.post.findMany({where: whereCondition})
+    
+    return Response.json(posts);
+}
--- src/components/InfiniteFeed.tsx
+++ src/components/InfiniteFeed.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-const InfiniteFeed = () => {
+const InfiniteFeed = ({ userProfileId }: { userProfileId?: string }) => {
   return (
     <div>InfiniteFeed</div>
   )
